55f452102dd485cd183f9363cd300b0b2680c2e8
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-04-26 20:54:17 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -70,8 +70,9 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
+new_value="Lemons"
 matrix_update= matrix[1][1]
-               matrix[1][1]= "new_value"
+               matrix[1][1]= "Lemons"
                matrix
 
 
